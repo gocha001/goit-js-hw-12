bd1edd08ae27c31880e250258721437f4a49c035
gocha001 <kalianichgocha@ukr.net>
2024-08-03 20:04:47 +0300

    add initial files

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default async function searchImagesByQuery(sear, page) {
+export default async function searchImagesByQuery(sear, page, per_page) {
   const response = await axios.get('https://pixabay.com/api/', {
     params: {
       key: '45094022-90f186f65f49c334b8043619f',
@@ -8,7 +8,7 @@ export default async function searchImagesByQuery(sear, page) {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      per_page: '15',
+      per_page: per_page,
       page: page,
     },
   });
--- src/main.js
+++ src/main.js
@@ -11,87 +11,107 @@ const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 const btnLoadMore = document.querySelector('.btn-load-more');
 
+const scrollUp = document.querySelector('.isShowBtn');
+scrollUp.style.display = 'none';
+scrollUp.addEventListener('click', handleScroll);
+function handleScroll() {
+  window.scrollTo(0, 0);
+}
+
+window.onscroll = () => {
+  if (window.scrollY > 700) {
+    scrollUp.style.display = 'block';
+  } else {
+    scrollUp.style.display = 'none';
+  }
+};
+
 btnLoadMore.style.display = 'none';
 
 loader.style.display = 'none';
 
 form.addEventListener('submit', handleSubmit);
-btnLoadMore.addEventListener('click', handleSubmitLoadMore);
 
+const per_page = 15;
 let sear = null;
 let page = 1;
-let totalHits = 0;
 
 async function handleSubmit(event) {
-  try {
-    event.preventDefault();
-    container.innerHTML = '';
-    page = 1;
-    btnLoadMore.style.display = 'none';
+  event.preventDefault();
+  container.innerHTML = '';
+  page = 1;
+  btnLoadMore.style.display = 'none';
 
-    const search = event.currentTarget.elements;
-    sear = search.text.value.trim().toLowerCase();
+  const search = event.currentTarget.elements;
+  sear = search.text.value.trim().toLowerCase();
 
-    if (sear == 0) {
-      myIziToast('The form field must be filled', '#abd4f8');
-      return;
-    }
-    loader.style.display = 'block';
-
-    await searchImagesByQuery(sear, page)
-      .then(data => {
-        totalHits = data.totalHits;
-        loader.style.display = 'none';
-        if (data.hits.length == 0) {
-          myIziToast(
-            'Sorry, there are no images matching your search query. Please try again!',
-            '#f28111'
-          );
-          return;
-        }
-        createImagesMarkup(data.hits);
-        btnLoadMore.style.display = 'block';
-      })
-      .catch(error => {
-        myIziToast('Oops... Something went wrong.', '#e97782');
-        loader.style.display = 'none';
-      })
-      .finally(() => form.reset());
-  } catch (error) {
-    myIziToast('Oops... Something went wrong.', '#e97782');
+  if (sear == 0) {
+    myIziToast('Warning', 'The form field must be filled', '#abd4f8');
+    return;
   }
-}
+  loader.style.display = 'block';
 
-async function handleSubmitLoadMore(event) {
   try {
-    event.preventDefault();
-    btnLoadMore.style.display = 'none';
-    page += 1;
-    if (totalHits < page * 15) {
-      loader.style.display = 'none';
+    const { hits, totalHits } = await searchImagesByQuery(sear, page, per_page);
+
+    loader.style.display = 'none';
+
+    if (hits.length == 0) {
       myIziToast(
-        'We re sorry, but you ve reached the end of search results.',
+        'Error',
+        'Sorry, there are no images matching your search query. Please try again!',
         '#f28111'
       );
       return;
     }
-    loader.style.display = 'block';
+    createImagesMarkup(hits);
 
-    await searchImagesByQuery(sear, page)
-      .then(data => {
-        console.log(data);
-        loader.style.display = 'none';
-
-        createImagesMarkup(data.hits);
-        btnLoadMore.style.display = 'block';
-        setScrolling();
-      })
-      .catch(error => {
-        myIziToast('Oops... Something went wrong.', '#e97782');
-        loader.style.display = 'none';
-      });
+    if (totalHits > per_page) {
+      btnLoadMore.addEventListener('click', handleSubmitLoadMore);
+      btnLoadMore.style.display = 'block';
+    } else {
+      myIziToast(
+        'Info',
+        'We re sorry, but you ve reached the end of search results.',
+        '#f28111'
+      );
+    }
   } catch (error) {
-    myIziToast('Oops... Something went wrong.', '#e97782');
+    myIziToast('Error', 'Oops... Something went wrong.', '#e97782');
+    loader.style.display = 'none';
+  } finally {
+    form.reset();
+  }
+}
+
+async function handleSubmitLoadMore(event) {
+  event.preventDefault();
+  btnLoadMore.style.display = 'none';
+  page += 1;
+  loader.style.display = 'block';
+
+  try {
+    const { hits, totalHits } = await searchImagesByQuery(sear, page, per_page);
+
+    loader.style.display = 'none';
+
+    createImagesMarkup(hits);
+    setScrolling();
+
+    const totalPage = Math.ceil(totalHits / per_page);
+    if (totalPage == page) {
+      myIziToast(
+        'Info',
+        'We re sorry, but you ve reached the end of search results.',
+        '#f28111'
+      );
+      btnLoadMore.removeEventListener('click', handleSubmitLoadMore);
+      return;
+    } else {
+      btnLoadMore.style.display = 'block';
+    }
+  } catch (error) {
+    myIziToast('Error', 'Oops... Something went wrong.', '#e97782');
   }
 }
 
@@ -102,9 +122,9 @@ function setScrolling() {
   });
 }
 
-function myIziToast(message, color) {
+function myIziToast(title, message, color) {
   iziToast.error({
-    title: 'Error',
+    title: `${title}`,
     message: `${message}`,
     position: 'topRight',
     transitionIn: 'bounceInDown',
